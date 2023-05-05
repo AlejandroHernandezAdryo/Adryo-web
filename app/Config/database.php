2ea6d9be905dc10cb280dcac828f1d38b67438c2
e-mejia-adryo <e.mejia@adryo.com.mx>
2023-05-05 15:15:19 -0600

    dos-commmt

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,22 +1,33 @@
 <?php
 class DATABASE_CONFIG {
         
-        public $default = array(
-                'datasource' => 'Database/Mysql',
-                'persistent' => true,
-                'host'       => 'localhost',
-                'login'      => 'root',
-                'password'   => 'root',
-                'database'   => 'inmosystem',
-                // 'encoding'   => 'UTF8',
-        );
-
-        // public $default = array(
-        //         'datasource' => 'Database/Mysql',
-        //         'persistent' => false,
-        //         'host' => 'inmosystem.cnhu5xayixb1.us-east-1.rds.amazonaws.com',
-        //         'login' => 'admina',
-        //         'password' => 'adminAigel',
-        //         'database' => 'inmosystem'
-        // );
+        //Localhost
+public $default = array(
+  'datasource' => 'Database/Mysql',
+  'persistent' => false,
+  'host' => 'localhost',
+  'login' => 'root',
+  'password' => '',
+  'database' => 'inmosystem'
+);
+        
+// RDS AWS Amazon
+/*public $default = array(
+  'datasource' => 'Database/Mysql',
+  'persistent' => false,
+  'host' => 'inmosystem.cnhu5xayixb1.us-east-1.rds.amazonaws.com',
+  'login' => 'admina',
+  'password' => 'adminAigel',
+  'database' => 'inmosystem'
+);*/
+        
+        //Dragon
+        /*public $default = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
+        'host' => 'localhost',
+        'login' => 'inmosystem',
+        'password' => 'Inmosystem.2018',
+        'database' => 'inmosystem'
+	);*/
 }
